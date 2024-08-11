24d267d0638aa1e658cbad05f4d9b8ff92345c1f
Ram Prasad <137366329+ram0109@users.noreply.github.com>
2024-08-11 10:05:01 +0530

    Update paymentController.js

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -36,7 +36,7 @@ export  const paymentVerification = async (req, res)=>{
               razorpay_signature
             })
 
-            res.redirect (`http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`)
+            res.redirect (`https://razorpay-integration-frontend.onrender.com/paymentsuccess?reference=${razorpay_payment_id}`)
         }
         else
         {
